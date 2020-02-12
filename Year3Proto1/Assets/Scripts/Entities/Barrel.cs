17af6da910a61ec636dc72ab3c190cef6c380e52
iHusker <ihuskypuppy@gmail.com>
2020-02-12 15:50:30 +1300

    Added Barrel Entity

---
+++ Year3Proto1/Assets/Scripts/Entities/Barrel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Barrel : MonoBehaviour
+{
+    private readonly float totalFuel;
+    private float currentFuel;
+
+    private bool interacting = false;
+
+    private Text text;
+
+    private void Start()
+    {
+        currentFuel = totalFuel;
+    }
+
+    public void Update()
+    {
+        if(interacting)
+        {
+            currentFuel -= Time.deltaTime;
+        }
+    }
+
+}
---
+++ Year3Proto1/Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour
+{
+    private List<int> layers;
+
+    private void Awake()
+    {
+        layers = new List<int>();
+    }
+
+    public abstract void OnInteraction(GameObject gameObject);
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObject = collision.gameObject;
+        layers.ForEach(layer => {
+            if(gameObject.layer == layer) OnInteraction(gameObject);
+        });
+    }
+}
