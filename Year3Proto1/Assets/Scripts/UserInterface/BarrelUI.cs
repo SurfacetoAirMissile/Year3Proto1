414df27decf8b2aa68242f3cafb09285d501a273
iHusker <60858984+iHusker@users.noreply.github.com>
2020-02-13 11:52:42 +1300

    Barrel UI update...

--- Year3Proto1/Assets/Scripts/UserInterface/BarrelUI.cs
+++ Year3Proto1/Assets/Scripts/UserInterface/BarrelUI.cs
@@ -7,9 +7,34 @@ using TMPro;
 public class BarrelUI : MonoBehaviour
 {
     public TMP_Text tmpText;
+    public Image image;
+
+    public Transform barrel;
+    private CanvasGroup canvasGroup;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
 
     private void Update()
     {
-        transform.LookAt(2 * transform.position - Camera.main.transform.position);
+        /*
+        float yOffSet = barrel.transform.position.y + 1.5f;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(barrel.position);
+
+        Transform textTransform = tmpText.transform;
+        Transform imageTransform = image.transform;
+
+        float alpha = (textTransform.position.z < 0) ? 0.0f : 1.0f;
+        canvasGroup.alpha = alpha;
+        bool imageIsActive = (imageTransform.position.z < 0) ? true : false;
+
+
+
+        transform.position = new Vector3(screenPoint.x, screenPoint.y + yOffSet, screenPoint.x);
+        */
+
+        //transform.LookAt(2 * transform.position - Camera.main.transform.position);
     }
 }
