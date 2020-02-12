575ad283f905a5a876543f953efcbc22cd4aff25
iHusker <ihuskypuppy@gmail.com>
2020-02-12 19:15:18 +1300

    Update Barrel Entity
    + User Interface
    + Parameters

--- Year3Proto1/Assets/Scripts/Entities/Barrel.cs
+++ Year3Proto1/Assets/Scripts/Entities/Barrel.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class Barrel : MonoBehaviour
 {
+    //Object Data
+    private bool interacting = true;
     private readonly float totalFuel;
     private float currentFuel;
 
-    private bool interacting = false;
+    //User Interface
+    [SerializeField] private GameObject userInterface;
+    [SerializeField] private Transform userInterfaceParent;
+    private GameObject userInterfaceFinal;
 
-    private Text text;
 
     private void Start()
     {
@@ -19,9 +23,25 @@ public class Barrel : MonoBehaviour
 
     public void Update()
     {
-        if(interacting)
+        if(interacting) 
         {
-            currentFuel -= Time.deltaTime;
+            if (userInterfaceFinal == null)
+            {
+                userInterfaceFinal = Instantiate(userInterface, userInterfaceParent, true);
+                userInterfaceFinal.GetComponent<BarrelUI>().SetBarrel(transform);
+            }
+            else
+            {
+                userInterfaceFinal.GetComponent<BarrelUI>().text.text = currentFuel + "/" + totalFuel;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetType() == typeof(Player))
+        {
+
         }
     }
 
---
+++ Year3Proto1/Assets/Scripts/UserInterface/BarrelUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarrelUI : MonoBehaviour
+{
+    public Image image;
+    public Text text;
+
+
+    private Transform parent;
+    private Transform barrel;
+
+    private void Start()
+    {
+        parent = GetComponentInParent<Transform>();
+    }
+
+    private void Update()
+    {
+        float yOffSet = barrel.transform.position.y + 1.5f;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(parent.position);
+
+        Transform textTransform = text.transform;
+        Transform imageTransform = image.transform;
+
+        textTransform.position = new Vector3(screenPoint.x, screenPoint.y + yOffSet, screenPoint.x);
+        imageTransform.position = new Vector3(screenPoint.x, screenPoint.y + yOffSet, screenPoint.x);
+    }
+}
