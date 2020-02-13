90b572cfdd2678b5db5375057ba2c8b52d8ae31e
iHusker <60858984+iHusker@users.noreply.github.com>
2020-02-14 10:37:30 +1300

    Added Abstraction for Interactables

---
+++ Year3Proto1/Assets/Scripts/UserInterface/InteractableUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableUI : MonoBehaviour
+{
+    public string displayname;
+    public float level = 0.0f;
+
+    public GameObject prompt;
+    public GameObject dot;
+    public GameObject progress;
+
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(target.position);
+        prompt.transform.position = screenPoint;
+        dot.transform.position = screenPoint;
+
+        float distanceFromObject = Vector3.Distance(Camera.main.transform.position, target.position);
+
+        prompt.SetActive(screenPoint.z > 0.0f && distanceFromObject < 2.0f);
+        dot.SetActive(screenPoint.z > 0.0f && distanceFromObject > 2.0f);
+    }
+}
