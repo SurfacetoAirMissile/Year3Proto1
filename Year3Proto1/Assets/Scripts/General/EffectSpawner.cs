2b97b09b51e5568971e973100be7524e7f3211e2
David Morris <pixelgrapher@outlook.co.nz>
2020-02-14 09:57:29 +1300

    Wind cannon effect done

---
+++ Year3Proto1/Assets/Scripts/General/EffectSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSpawner : MonoBehaviour
+{
+
+    public GameObject effect;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SpawnEffect();
+        }
+    }
+
+    public void SpawnEffect()
+    {
+        if (effect != null)
+        {
+            Instantiate(effect, transform);
+        }
+        else
+        {
+            Debug.LogError("Effect not set in inspector");
+        }
+    }
+}
