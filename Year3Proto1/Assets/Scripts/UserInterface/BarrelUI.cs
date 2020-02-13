11c2aeb9106613ed9b0e0b7cb573230104a895cb
iHusker <ihuskypuppy@gmail.com>
2020-02-13 19:25:28 +1300

    Fixed WorldToScreenPoint

--- Year3Proto1/Assets/Scripts/UserInterface/BarrelUI.cs
+++ Year3Proto1/Assets/Scripts/UserInterface/BarrelUI.cs
@@ -8,33 +8,17 @@ public class BarrelUI : MonoBehaviour
 {
     public TMP_Text tmpText;
     public Image image;
-
     public Transform barrel;
-    private CanvasGroup canvasGroup;
-
-    private void Start()
-    {
-        canvasGroup = GetComponent<CanvasGroup>();
-    }
 
     private void Update()
     {
-        /*
-        float yOffSet = barrel.transform.position.y + 1.5f;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(barrel.position);
+        image.transform.position = screenPoint;
+        tmpText.transform.position = screenPoint;
 
-        Transform textTransform = tmpText.transform;
-        Transform imageTransform = image.transform;
+        image.gameObject.SetActive(screenPoint.z > 0);
+        tmpText.gameObject.SetActive(screenPoint.z > 0);
 
-        float alpha = (textTransform.position.z < 0) ? 0.0f : 1.0f;
-        canvasGroup.alpha = alpha;
-        bool imageIsActive = (imageTransform.position.z < 0) ? true : false;
-
-
-
-        transform.position = new Vector3(screenPoint.x, screenPoint.y + yOffSet, screenPoint.x);
-        */
-
-        //transform.LookAt(2 * transform.position - Camera.main.transform.position);
+        //transform.LookAt(2 * barrel.position - Camera.main.transform.position);
     }
 }
