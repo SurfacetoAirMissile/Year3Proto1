aaf375eecba4a258705800f7f88d3b128f3c1d43
iHusker <60858984+iHusker@users.noreply.github.com>
2020-02-21 12:58:05 +1300

    Trading System Update

---
+++ Year3Proto1/Assets/Scripts/UserInterface/TradeCategory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TradeCategory : MonoBehaviour
+{
+    public string categoryName;
+    public Image image;
+    public Transform parent;
+
+    private List<ResourceEntity> resourceEntities;
+    private int index = 0;
+    private int playerMoney = 100;
+
+    private void Start()
+    {
+        resourceEntities = new List<ResourceEntity>();
+
+        foreach (ResourceEntity resourceEntity in Resources.LoadAll<ResourceEntity>("Trading/" + categoryName))
+        {
+            resourceEntities.Add(Instantiate(resourceEntity, parent, false));
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            if (index >= (resourceEntities.Count - 1))
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+
+            image.transform.DOLocalMoveY(resourceEntities[index].transform.localPosition.y, 0.2f).SetEase(Ease.OutQuint);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (index <= 0)
+            {
+                index = (resourceEntities.Count - 1);
+            } 
+            else
+            {
+                index--;
+            }
+
+            image.transform.DOLocalMoveY(resourceEntities[index].transform.localPosition.y, 0.2f).SetEase(Ease.OutQuint);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(playerMoney >= resourceEntities[index].price) resourceEntities[index].unityEvent.Invoke();
+        }
+    }
+
+    public ResourceEntity GetCurrentResource()
+    {
+        return resourceEntities[index];
+    }
+}
---
+++ Year3Proto1/Assets/Scripts/UserInterface/Trading.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trading : MonoBehaviour
+{
+    public List<string> categoryNames;
+    public GameObject categoryPrefab;
+
+    private List<TradeCategory> tradeCategories;
+    private int index = 0;
+
+    private void Start()
+    {
+        tradeCategories = new List<TradeCategory>();
+        foreach(string categoryName in categoryNames)
+        {
+            GameObject category = Instantiate(categoryPrefab, transform, false);
+            TradeCategory tradeCategory = category.GetComponent<TradeCategory>();
+            if (tradeCategory != null)
+            {
+                tradeCategory.categoryName = categoryName;
+                tradeCategory.gameObject.SetActive(false);
+            }
+            else
+            {
+                Destroy(category);
+            }
+
+            tradeCategories.Add(tradeCategory);
+        }
+
+        tradeCategories[0].gameObject.SetActive(true);
+    }
+}
